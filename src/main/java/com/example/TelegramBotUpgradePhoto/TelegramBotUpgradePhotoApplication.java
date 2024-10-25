92c70a547c9b2e7a012747a04b43bc288e3a2b44
DenWays <130283554+DenWays@users.noreply.github.com>
2024-10-25 15:33:09 +0500

    Create project

---
+++ src/main/java/com/example/TelegramBotUpgradePhoto/TelegramBotUpgradePhotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.TelegramBotUpgradePhoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotUpgradePhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotUpgradePhotoApplication.class, args);
+	}
+
+}
